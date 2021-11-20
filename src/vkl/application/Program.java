832f42302df4d83900a38a49cece4788b22b1352
viktor-lima <joaolimak10@outlook.com>
2021-11-20 18:45:00 -0300

    Imprimi tabuleiro

---
+++ src/vkl/application/Program.java
@@ -0,0 +1,12 @@
+package vkl.application;
+
+import vkl.puzzle.Impl;
+
+public class Program {
+	public static void main(String[] args) {
+		
+		Impl.inicializaVariaveis();
+		Impl.imprimeTabuleiroBonito();
+		
+	}
+}
---
+++ src/vkl/puzzle/Impl.java
@@ -0,0 +1,72 @@
+package vkl.puzzle;
+
+public class Impl {
+	
+	static int [][] tabuleiro;
+	static int movimento = 0;
+	//coordenadas do espaço em branco
+	static int xBranco = 0;
+	static int yBranco = 0;
+	
+	public static void inicializaVariaveis() {
+		int k = 0;
+		tabuleiro = new int[3][3];
+		for (int i = 0; i < tabuleiro.length; i++) {
+			for (int j = 0; j < tabuleiro[i].length; j++) {
+				k++;
+				tabuleiro[i][j] = k;
+			}
+		}
+		
+		//initial position white
+		xBranco = 2;
+		yBranco = 2;
+		movimento=0;
+	}
+	
+	public static void imprimiTabuleiro() {
+		for (int i = 0; i < tabuleiro.length; i++) {
+			for (int j = 0; j < tabuleiro[i].length; j++) {
+				if(tabuleiro[i][j] != 9)
+					System.out.printf("%d\t",tabuleiro[i][j]);
+				else
+					System.out.printf(" \t");
+			}
+			System.out.printf("\n");
+		}
+	}
+	
+	public static void imprimeTabuleiroBonito() {
+		int[] state = new int[9];
+		String result = "";
+		String[] imprimiIsto = {"","","","","","","","",""};
+		
+		for (int i = 0; i < tabuleiro.length; i++) {
+			for (int j = 0; j < tabuleiro[i].length; j++) {
+				state[3*i+j] = tabuleiro[i][j];
+			}
+		}
+		
+		for (int i = 0; i < state.length; i++) {
+			String insertString = "|";
+			if (state[i] == 9) 
+				insertString+=" ";
+			else
+				insertString+= state[i]+" ";
+			
+			imprimiIsto[i] = insertString;	
+		}
+		
+		result+=" ________\n"+"|  |  |  |\n" +imprimiIsto[0]+imprimiIsto[1]+imprimiIsto[2]
+				+"|\n"+"|__|__|__|\n"+"|  |  |  |\n"+
+				imprimiIsto[3]+imprimiIsto[4]+imprimiIsto[5]+"|\n"+
+				"|__|__|__|\n"+"|  |  |  |\n" +
+				imprimiIsto[6]+imprimiIsto[7]+imprimiIsto[8]+" | \n"+"|__|__|__|";
+		
+		System.out.println(result);
+				
+		
+	}
+	
+	
+}
